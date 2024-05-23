61ec6d7c22502ab4db2acb28f4354a2f3f0be311
unknown <tanjimbinamin@gmail.com>
2024-05-23 18:56:59 +0600

    router created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from "express";
-
+import { productsRoute } from "./app/modules/Product/product.routes";
+import { orderRoute } from "./app/modules/order/order.routes";
 
 const app = express();
 
 app.use(express.json());
 
+// For product
+app.use("/api/products", productsRoute);
 
+// For order
+app.use("/api/orders", orderRoute);
 
 app.use((req, res) => {
   res.status(500).json({
